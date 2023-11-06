53a02569d9d140f4e19047085df32720f213f319
Kirilloao <kirilltaraturin747@gmail.com>
2023-11-06 16:51:31 +0100

    Initial Commit

---
+++ ThreeView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThreeView
+//
+//  Created by Kirill Taraturin on 06.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
